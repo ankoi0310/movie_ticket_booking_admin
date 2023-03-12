1ce2345e4a75740758a528033e19637b1b583a1a
ankoi0310 <huynhvahuuan3620@gmail.com>
2023-03-12 23:56:40 +0700

    update dashboard screen

--- lib/screen/dashboard/dashboard_screen.dart
+++ lib/screen/dashboard/dashboard_screen.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/component/activity.dart';
 import 'package:movie_ticket_booking_admin_flutter_nlu/component/bar_chart.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/component/info_card.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/config/responsive.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/config/size_config.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/service/statistic_service.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/style/colors.dart';
-import 'package:movie_ticket_booking_admin_flutter_nlu/style/style.dart';
+import 'package:movie_ticket_booking_admin_flutter_nlu/core.dart';
+import 'package:movie_ticket_booking_admin_flutter_nlu/layout/default/component/activity.dart';
+import 'package:movie_ticket_booking_admin_flutter_nlu/screen/dashboard/component/general_statistic.dart';
 
 class DashboardScreen extends StatefulWidget {
-  static const routeName = '/dashboard';
   const DashboardScreen({Key? key}) : super(key: key);
 
   @override
@@ -20,40 +14,9 @@ class DashboardScreen extends StatefulWidget {
 class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
-    StatisticService statisticService = StatisticService(context);
-    statisticService.getStatistic();
     return Column(
       children: [
-        SizedBox(
-          width: SizeConfig.screenWidth,
-          child: Wrap(
-            spacing: 20,
-            runSpacing: 20,
-            alignment: WrapAlignment.spaceBetween,
-            children: [
-              const InfoCard(
-                icon: 'icons/branch.svg',
-                label: 'Chi nhánh \nHoạt Động',
-                amount: '15',
-              ),
-              const InfoCard(
-                icon: 'icons/credit-card.svg',
-                label: 'Phim \nĐang Chiếu',
-                amount: '12',
-              ),
-              const InfoCard(
-                icon: 'icons/showtime.svg',
-                label: 'Suất Chiếu \nTrong Ngày',
-                amount: '32',
-              ),
-              InfoCard(
-                icon: 'icons/salary.svg',
-                label: 'Lợi Nhuận \nTrong Ngày',
-                amount: NumberFormat.currency(locale: 'vi', symbol: 'VNĐ').format(15000000),
-              ),
-            ],
-          ),
-        ),
+        const GeneralStatistic(),
         SizedBox(height: SizeConfig.blockSizeVertical * 4),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
